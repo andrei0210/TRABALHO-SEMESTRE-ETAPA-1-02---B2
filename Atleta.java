460e6efade2803661a5749ba681d6d4db0bb22e8
Andrei <160683237+andrei0210@users.noreply.github.com>
2024-10-23 21:16:00 -0300

    Create Atleta.java

---
+++ Atleta.java
@@ -0,0 +1,9 @@
+public class Atleta {
+    private String id;
+    private String nome;
+    private String email;
+    private String esporte;
+    private String historico;
+
+    // Construtores, getters e setters
+}
